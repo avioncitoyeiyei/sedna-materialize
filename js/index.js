ec782c24a45ea5d73746af8c3af52fab32133f5c
avioncitoyeiyei <yeisonferley08@gmail.com>
2018-09-19 07:09:32 -0500

    cambios

--- js/index.js
+++ js/index.js
@@ -3,3 +3,29 @@ window.addEventListener('scroll', () =>{
     if(window.scrollY>0) navBar.classList.add('is-down')
     else navBar.classList.remove('is-down')
 })
+AOS.init();
+
+
+$('.lightbox').hide();
+$('#open').click(function(){
+    $('.lightbox').show();
+})
+$('#close1').click(function(){
+    $('.lightbox').hide();
+})
+
+$('.lightbox2').hide();
+$('#open2').click(function(){
+    $('.lightbox2').show();
+})
+$('#close2').click(function(){
+    $('.lightbox2').hide();
+})
+
+$('.lightbox3').hide();
+$('#open3').click(function(){
+    $('.lightbox3').show();
+})
+$('#close3').click(function(){
+    $('.lightbox3').hide();
+})
